b89828ec46c202df42ef0bd54fdedfc32ace4498
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2023-11-26 12:47:53 -0500

    insert random commit message here

--- src/domstuff.tsx
+++ src/domstuff.tsx
@@ -1,14 +1,64 @@
+import * as monaco from "monaco-editor";
+
+let currentFile: FileSystemFileHandle;
+let currentFolder: FileSystemDirectoryHandle;
+
+function clearModels() {
+  monaco.editor.getModels().forEach((model) => model.dispose());
+}
+
+async function openFile() {
+  const [handle] = await showOpenFilePicker();
+
+  if (await readWritePermission(handle)) {
+    currentFile = handle;
+    const file = await handle.getFile();
+    const text = await file.text();
+
+    clearModels();
+    const fileModel = monaco.editor.createModel(
+      text,
+      undefined,
+      new monaco.Uri().with({ path: file.name })
+    );
+
+    editor.setModel(fileModel);
+  } else alert("Error: Read/write access denied. Please try again.");
+}
+
+async function save() {
+  const writer = (await currentFile.createWritable()).getWriter();
+  writer.write(monaco.editor.getModel(new monaco.Uri()));
+}
+
+window.addEventListener("keydown", ({ repeat, key, ctrlKey }) => {
+  if (!repeat && ctrlKey && key === "s") save();
+});
+
+async function readWritePermission(handle: FileSystemHandle) {
+  let permission = await handle.queryPermission({ mode: "readwrite" });
+  if (permission == "granted") return true;
+
+  permission = await handle.requestPermission({ mode: "readwrite" });
+  if (permission == "granted") return true;
+
+  return false;
+}
+
+const editorDiv = <div id="editor"></div>;
+
+const editor = monaco.editor.create(editorDiv, {
+  theme: "vs-dark",
+  automaticLayout: true,
+});
+
 const app = (
   <div id="app">
     <div id="sidebar">
-      <button
-        onclick={() => {
-          alert("hi");
-        }}
-      >
-        hi
-      </button>
+      <button on:click={() => openFile()}>Open File</button>
+      <button on:click={() => alert("hi")}>Open Folder</button>
     </div>
+    {editorDiv}
   </div>
 );
 
