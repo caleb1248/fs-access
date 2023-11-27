d06e07b4ecacd0ba8b02346044383f530aeedaee
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2023-11-26 19:42:47 -0500

    saving

--- src/domstuff.tsx
+++ src/domstuff.tsx
@@ -1,6 +1,35 @@
 import * as monaco from "monaco-editor";
 
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+
+self.MonacoEnvironment = {
+  getWorker: function (_: string, label: string) {
+    switch (label) {
+      case "json":
+        return new jsonWorker();
+      case "css":
+      case "scss":
+      case "less":
+        return new cssWorker();
+      case "html":
+      case "handlebars":
+      case "razor":
+        return new htmlWorker();
+      case "typescript":
+      case "javascript":
+        return new tsWorker();
+      default:
+        return new editorWorker();
+    }
+  },
+};
+
 let currentFile: FileSystemFileHandle;
+let currentPath: string;
 let currentFolder: FileSystemDirectoryHandle;
 
 function clearModels() {
@@ -14,6 +43,7 @@ async function openFile() {
     currentFile = handle;
     const file = await handle.getFile();
     const text = await file.text();
+    currentPath = file.name;
 
     clearModels();
     const fileModel = monaco.editor.createModel(
@@ -21,18 +51,44 @@ async function openFile() {
       undefined,
       new monaco.Uri().with({ path: file.name })
     );
-
     editor.setModel(fileModel);
   } else alert("Error: Read/write access denied. Please try again.");
 }
 
-async function save() {
-  const writer = (await currentFile.createWritable()).getWriter();
-  writer.write(monaco.editor.getModel(new monaco.Uri()));
+async function openFolder() {
+  const handle = await showDirectoryPicker();
+
+  if (await readWritePermission(handle)) {
+    currentFolder = handle;
+    const stuff = handle.values();
+    for await (const value of stuff) {
+      console.log(value);
+    }
+    clearModels();
+    // const fileModel = monaco.editor.createModel(
+    //   text,
+    //   undefined,
+    //   new monaco.Uri().with({ path: file.name })
+    // );
+    // editor.setModel(fileModel);
+  } else alert("Error: Read/write access denied. Please try again.");
 }
 
-window.addEventListener("keydown", ({ repeat, key, ctrlKey }) => {
-  if (!repeat && ctrlKey && key === "s") save();
+async function save() {
+  if (!currentFile) throw "No current file";
+  const currentModel = editor.getModel();
+  if (!currentModel) throw "No current model";
+
+  const writer = (await currentFile.createWritable()).getWriter();
+  writer.write(currentModel.getValue());
+  writer.close();
+}
+
+window.addEventListener("keydown", (e) => {
+  if (!e.repeat && e.ctrlKey && e.key === "s") {
+    e.preventDefault();
+    save();
+  }
 });
 
 async function readWritePermission(handle: FileSystemHandle) {
@@ -52,11 +108,13 @@ const editor = monaco.editor.create(editorDiv, {
   automaticLayout: true,
 });
 
+clearModels();
+
 const app = (
   <div id="app">
     <div id="sidebar">
       <button on:click={() => openFile()}>Open File</button>
-      <button on:click={() => alert("hi")}>Open Folder</button>
+      <button on:click={() => openFolder()}>Open Folder</button>
     </div>
     {editorDiv}
   </div>
