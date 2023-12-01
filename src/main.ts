c081ff31ffd830e28283233eb8154c2f3555746f
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2023-12-01 17:17:40 -0500

    remane 'spitpane' to 'splitpane'

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import "./lib/jsxfactory";
 import "./style.css";
 import "xterm/css/xterm.css";
 import app from "./domstuff";
-import "./lib/spitpane/splitpane";
+import "./lib/splitpane/splitpane";
 
 document.body.appendChild(app);
